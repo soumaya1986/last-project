9e5394eb28538ed3eec1eb6b70e03debc7e7ded5
soumaya1986 <soumaya.chamekh@yahoo.com>
2024-09-30 15:52:45 +0200

    Create index.js

---
+++ client/src/store/common-slice/index.js
@@ -0,0 +1,45 @@
+import { API_URL } from "@/config";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  featureImageList: [],
+};
+
+export const getFeatureImages = createAsyncThunk(
+  "/order/getFeatureImages",
+  async () => {
+    const response = await axios.get(
+      `${API_URL}/common/feature/get`
+    );
+
+    return response.data;
+  }
+);
+
+
+
+
+
+const commonSlice = createSlice({
+  name: "commonSlice",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getFeatureImages.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getFeatureImages.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.featureImageList = action.payload.data;
+      })
+      .addCase(getFeatureImages.rejected, (state) => {
+        state.isLoading = false;
+        state.featureImageList = [];
+      });
+  },
+});
+
+export default commonSlice.reducer;
